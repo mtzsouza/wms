20db2615f2b67526c1b453c1f1d9ff349041d8bd
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2024-04-10 11:17:19 -0500

    Hiding environments from github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: "AIzaSyD1eZP-52ZG5l5l8ucZ-45kKK0HL7VSlts",
-        authDomain: "wmsystem-c3633.firebaseapp.com",
-        projectId: "wmsystem-c3633",
-        storageBucket: "wmsystem-c3633.appspot.com",
-        messagingSenderId: "1051689854635",
-        appId: "1:1051689854635:web:1e97e3f2e8e396af8579ee"
+        apiKey: "placeholder",
+        authDomain: "placeholder",
+        projectId: "placeholder",
+        storageBucket: "placeholder",
+        messagingSenderId: "placeholder",
+        appId: "placeholder"
     }
   };
